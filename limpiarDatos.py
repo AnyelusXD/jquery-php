9667cb08f09eca6c206c39d36d584f3cd3263840
AnyelusXD <64674612+AnyelusXD@users.noreply.github.com>
2022-08-16 08:09:49 -0400

    Update limpiarDatos.py

--- limpiarDatos.py
+++ limpiarDatos.py
@@ -1,9 +1,23 @@
+#importamos Pandas
 import pandas as pd
 
-excel1 = "dataframe/counties.xlsx"
-df = pd.read_excel(excel1)
+#cargamos hoja excel en una variable
+data = "counties.xlsx"
 
-print (df)
-for column in df.columns:
-    df [column] = df [column].str.replace(r"\W","")
-df.to_excel("dataframe/excellimpio.xlsx")
+#leemos el excel
+df = pd.read_excel(data)
+
+#quitamos caracteres especiales 
+df['county'] = df['county'].str.replace(r"\W","", regex=True)
+
+#asignamos el indice
+df_Clean=df.set_index('codestate')
+
+#guardamos los datos limpios en nuevo archivo excel
+df_Clean.to_excel("DataClean.xlsx")
+
+#creamos un archivo csv para insertar datos en la BD
+df_Clean.to_csv('counties.csv', encoding='utf-8')
+ 
+
+print("Dataset Clean")
